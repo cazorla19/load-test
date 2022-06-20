977e2a98351119a694c203b5b85dd2d69bf9036a
cazorla19 <michael.chinkov@gmail.com>
2022-06-20 17:10:11 +0200

    add working load test

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,19 @@
 import base64
 
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, task
 from random import randint, choice
 
 
-class WebTasks(TaskSet):
+class WebUser(HttpUser):
 
     @task
     def load(self):
-        base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
-
         catalogue = self.client.get("/catalogue").json()
         category_item = choice(catalogue)
         item_id = category_item["id"]
 
         self.client.get("/")
-        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
+        self.client.get("/login", headers={"Authorization":"Basic Y2F6b3JsYTE5OjEyMw=="})
         self.client.get("/category.html")
         self.client.get("/detail.html?id={}".format(item_id))
         self.client.delete("/cart")
@@ -23,8 +21,3 @@ class WebTasks(TaskSet):
         self.client.get("/basket.html")
         self.client.post("/orders")
 
-
-class Web(HttpLocust):
-    task_set = WebTasks
-    min_wait = 0
-    max_wait = 0
